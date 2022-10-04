6e0b16b1a5a227a5683f3413f811be1d6993cd07
twati2 <104117914+twati2@users.noreply.github.com>
2022-10-04 09:00:05 +0100

    from monolithe to microservice

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=admin2
+export POSTGRES_PASSWORD=azerty789
+export POSTGRES_HOST=udagram-instance2.ctrhpsuq1kmj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=udagram-micro
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=bonjour
+export URL=http://localhost:8100
