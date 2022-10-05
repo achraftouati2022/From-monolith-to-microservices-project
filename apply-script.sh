7406a3dd8d57eb203b4ad0b21e414dcdf4715a3d
twati2 <104117914+twati2@users.noreply.github.com>
2022-10-05 11:01:22 +0100

    exporting to external IP 5

--- apply-script.sh
+++ apply-script.sh
@@ -2,3 +2,7 @@ kubectl apply -f  user-deployment.yml
 kubectl apply -f  feed-deployment.yml
 kubectl apply -f  reverseproxy-deployment.yml
 kubectl apply -f  frontend-deployment.yml
+
+kubectl apply -f  aws-creds.yml
+kubectl apply -f  env-configmap.yml
+kubectl apply -f  env-secret.yml
