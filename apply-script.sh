f5462af1f00a2a09bf24a76dd11e58fb0bc18a05
twati2 <104117914+twati2@users.noreply.github.com>
2022-10-05 19:12:14 +0100

    ready to submit finally

--- apply-script.sh
+++
@@ -1,8 +0,0 @@
-kubectl apply -f  user-deployment.yml
-kubectl apply -f  feed-deployment.yml
-kubectl apply -f  frontend-deployment.yml
-kubectl apply -f  reverseproxy-deployment.yml
-
-kubectl apply -f  aws-creds.yml
-kubectl apply -f  env-configmap.yml
-kubectl apply -f  env-secret.yml
--- apply-services.sh
+++
@@ -1,4 +0,0 @@
-kubectl apply -f  user-service.yml
-kubectl apply -f  feed-service.yml
-kubectl apply -f  frontend-service.yml
-kubectl apply -f  reverseproxy-service.yml
